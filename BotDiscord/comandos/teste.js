ee2f65778618974aac1246871756c3ed17b461fd
Higor Camelo <higorcamelo@gmail.com>
2021-09-26 14:56:09 -0300

    Novo comando de ajuda

--- BotDiscord/comandos/teste.js
+++
@@ -1,9 +0,0 @@
-module.exports = { 
-    name: "teste",
-    descricao: "testa a funcionalidade dos args",
-    args: true,
-    
-    execute(mensagem, args){
-        mensagem.channel.send(`Argumentos: ${args} \n Tamanho dos argumentos: ${args.length}`);
-    },
-};
